f147e5a5e24c12394831e3ee1c39ce3e177e27a6
Netanel Friedman <netanel.friedman6@gmail.com>
2026-02-07 18:50:07 +0200

    Update test script to support multiple executables

--- run_tests.sh
+++ run_tests.sh
@@ -4,12 +4,22 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-TEST_EXE_PATH=/tmp/test
 TEST_DIR=tests
 
 run_test() {
-    clang -fcolor-diagnostics -std=c23 $1 -o $TEST_EXE_PATH
-    $TEST_EXE_PATH && printf "test $1 ${GREEN}OK${NC}\n" || printf "test $1 ${RED}ERROR${NC}\n"
+    set -e
+
+    SOURCE=$1
+    TARGET_NAME=$(basename $1 .c)
+    TARGET="/tmp/$TARGET_NAME"
+
+    clang -fcolor-diagnostics -std=c23 $SOURCE -o $TARGET
+
+    $TARGET &&
+        printf "test $TARGET_NAME ${GREEN}OK${NC}\n" ||
+        printf "test $TARGET_NAME ${RED}ERROR${NC}\n"
+
+    rm $TARGET
 }
 
 for test_source_file in $(find $TEST_DIR -name "*.c")
